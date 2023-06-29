56ceb02b814a6757b6110387feb75816a2f0dba9
Raghu K <raghuk.vit@gmail.com>
2023-06-29 08:40:13 +0530

    Update tf-module-alb/

--- main.tf
+++ main.tf
@@ -38,6 +38,23 @@ resource "aws_lb" "main" {
   tags               = merge({ Name = "${var.name}-${var.env}-lb" }, var.tags)
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
   port              = 443
