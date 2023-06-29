458db67558f3684b243330746bf4d4e32c4b52ae
Raghu K <raghuk.vit@gmail.com>
2023-06-29 08:37:16 +0530

    Update tf-module-alb/

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_lb" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = var.port
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = "arn:aws:acm:us-east-1:739561048503:certificate/f5a87677-8328-4370-9bad-81c18d400f65"
