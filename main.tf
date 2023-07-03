8ae5dc8208574235b4f333e035a12aab822689ec
Raghu K <raghuk.vit@gmail.com>
2023-07-03 08:23:10 +0530

    Update tf-module-alb/

--- main.tf
+++ main.tf
@@ -38,7 +38,8 @@ resource "aws_lb" "main" {
   tags               = merge({ Name = "${var.name}-${var.env}-lb" }, var.tags)
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "public" {
+  count             = var.name == "public" ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -50,6 +51,23 @@ resource "aws_lb_listener" "http" {
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "private" {
+  count             = var.name == "private" ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default Error"
+      status_code  = "500"
     }
   }
 }
